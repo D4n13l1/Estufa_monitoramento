ceb8fda259731f8b5f4586b73fc11516c16e5d6a
Daniel Jorge de Abreu <88353850+D4n13l1@users.noreply.github.com>
2022-07-25 00:32:15 -0300

    Update Formulas.ino

--- Monitoramento/Formulas.ino
+++ Monitoramento/Formulas.ino
@@ -1,68 +1,72 @@
-double UA(float Temp, float UR) //UMIDADE ABSOLUTA
+double UA(double Temp, double UR) //UMIDADE ABSOLUTA
 {
-  return (UR * 1330 * pow(M_E, ((17.62 * Temp) / (Temp + 243.12)))) / (Temp + 273.16);
+  return ((UR/100) * 1330 * pow(M_E, ((17.62 * Temp) / (Temp + 243.12)))) / (Temp + 273.16);
 }
 
-double TS(float vazao, float UAint, float UAext) //Taxa de secagem
+double TS(double vazao, double UAint, double UAext) //Taxa de secagem
 {
   return (UAint - UAext) * vazao;
 }
 double getTempChan0() {
   selChan0();
   selChan0();
-  double b, c, x = hdc1080.readTemperature() , i = 0;
-  while (i < 10)
+  double b, c, x, i = 0;
+  while (i < 11)
   {
+    x = hdc1080.readTemperature();
     b = 0.95 * b + 0.05 * x;
     c = 0.95 * c + 0.05 * b;
-    x = hdc1080.readTemperature();
     i++;
   }
+  totTemp0 += tmpChan0;
   return c;
 }
 
 double getTempChan1() {
   selChan1();
   selChan1();
-  double b, c, x = hdc1080.readTemperature(), i = 0;
-  while (i < 10)
+  double b, c, x, i = 0;
+  while (i < 11)
   {
+    x = hdc1080.readTemperature();
     b = 0.99 * b + 0.01 * x;
     c = 0.97 * c + 0.03 * b;
-    x = hdc1080.readTemperature();
     i++;
   }
+  totTemp1 += tmpChan1;
   return c;
 }
 double getHumidChan0() {
   selChan0();
   selChan0();
   double b, c, x, i = 0;
-  while (i < 5) {
+  while (i < 7) {
     x = hdc1080.readHumidity();
-    b = 0.99 * b + 0.01 * x;
-    c = 0.97 * c + 0.03 * b;
+    b = 0.9 * b + 0.1 * x;
+    c = 0.9 * c + 0.1 * b;
     i++;
   }
+  totHum0 += hmdChan0;
   return c;
 }
 double getHumidChan1() {
   selChan1();
   selChan1();
   double b, c, x, i = 0;
-  while (i < 5) {
+  while (i < 7) {
     x = hdc1080.readHumidity();
-    b = 0.95 * b + 0.05 * x;
-    c = 0.95 * c + 0.05 * b;
+    b = 0.9 * b + 0.1 * x;
+    c = 0.9 * c + 0.1 * b;
     i++;
   }
+  totHum1 += hmdChan1;
   return c;
 }
-void turnSensorOn(){
+void turnSensorOn() {
   digitalWrite(43, HIGH);
   digitalWrite(33, HIGH);
 }
-void turnSensorOff(){
+void turnSensorOff() {
   digitalWrite(43, LOW);
   digitalWrite(33, LOW);
 }
